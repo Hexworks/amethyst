6b02e68e1f35e597f4be067e919c863a6deb494b
Adam Arold <arold.adam@gmail.com>
2020-11-23 23:34:22 +0100

    progress

--- amethyst.core/src/commonMain/kotlin/org/hexworks/amethyst/samples/StateMachineSample.kt
+++ amethyst.core/src/commonMain/kotlin/org/hexworks/amethyst/samples/StateMachineSample.kt
@@ -11,78 +11,78 @@ import org.hexworks.amethyst.api.message.StateChanged
 import org.hexworks.cobalt.core.api.UUID
 import kotlin.reflect.KClass
 
-object MyContext : Context
+class StateMachineSample {
 
-sealed class BarrierAction : Message<MyContext>
+    object MyContext : Context
 
-data class Unlock(
-        override val context: MyContext,
-        override val source: Entity<out EntityType, MyContext>
-) : BarrierAction()
+    sealed class BarrierAction : Message<MyContext> {
+        data class Unlock(
+                override val context: MyContext,
+                override val source: Entity<out EntityType, MyContext>
+        ) : BarrierAction()
 
-data class Open(
-        override val context: MyContext,
-        override val source: Entity<out EntityType, MyContext>
-) : BarrierAction()
+        data class Open(
+                override val context: MyContext,
+                override val source: Entity<out EntityType, MyContext>
+        ) : BarrierAction()
 
-data class Close(
-        override val context: MyContext,
-        override val source: Entity<out EntityType, MyContext>
-) : BarrierAction()
-
-object Unlockable : BaseFacet<MyContext, Unlock>(Unlock::class) {
-    override suspend fun receive(message: Unlock): Response {
-        return StateResponse(Openable)
+        data class Close(
+                override val context: MyContext,
+                override val source: Entity<out EntityType, MyContext>
+        ) : BarrierAction()
     }
-}
 
-object Openable : BaseFacet<MyContext, Open>(Open::class) {
-    override suspend fun receive(message: Open): Response {
-        return StateResponse(Closeable)
-    }
-}
-
-object Closeable : BaseFacet<MyContext, Close>(Close::class) {
-    override suspend fun receive(message: Close): Response {
-        return StateResponse(Openable)
-    }
-}
-
-object MyType : EntityType {
-    override val name = "Chest"
-    override val description = "A locked chest, holding precious items"
-    override val id = UUID.randomUUID()
-}
-
-data class HiddenInventory(
-        val items: Set<String>
-) : BaseAttribute()
-
-data class Inventory(
-        val items: Set<String>
-) : BaseAttribute()
-
-object InventoryHandler : BaseFacet<MyContext, StateChanged<MyContext>>(messageType = StateChanged::class as KClass<StateChanged<MyContext>>) {
-    override suspend fun receive(message: StateChanged<MyContext>): Response {
-        val entity = message.source.asMutableEntity()
-        return when (message.newState) {
-            is Closeable -> entity.findAttributeOrNull(HiddenInventory::class)?.let {
-                entity.removeAttribute(it)
-                entity.addAttribute(Inventory(it.items))
-                Consumed
-            } ?: Pass
-            is Openable -> entity.findAttributeOrNull(Inventory::class)?.let {
-                entity.removeAttribute(it)
-                entity.addAttribute(HiddenInventory(it.items))
-                Consumed
-            } ?: Pass
-            else -> Pass
+    object Unlockable : BaseFacet<MyContext, BarrierAction.Unlock>(BarrierAction.Unlock::class) {
+        override suspend fun receive(message: BarrierAction.Unlock): Response {
+            return StateResponse(Openable)
         }
     }
 
-}
+    object Openable : BaseFacet<MyContext, BarrierAction.Open>(BarrierAction.Open::class) {
+        override suspend fun receive(message: BarrierAction.Open): Response {
+            return StateResponse(Closeable)
+        }
+    }
 
-class StateMachineSample {
+    object Closeable : BaseFacet<MyContext, BarrierAction.Close>(BarrierAction.Close::class) {
+        override suspend fun receive(message: BarrierAction.Close): Response {
+            return StateResponse(Openable)
+        }
+    }
+
+    object MyType : EntityType {
+        override val name = "Chest"
+        override val description = "A locked chest, holding precious items"
+        override val id = UUID.randomUUID()
+    }
+
+    data class HiddenInventory(
+            val items: Set<String>
+    ) : BaseAttribute()
+
+    data class Inventory(
+            val items: Set<String>
+    ) : BaseAttribute()
+
+    object InventoryHandler : BaseFacet<MyContext, StateChanged<MyContext>>(messageType = StateChanged::class as KClass<StateChanged<MyContext>>) {
+        override suspend fun receive(message: StateChanged<MyContext>): Response {
+            val entity = message.source.asMutableEntity()
+            return when (message.newState) {
+                is Closeable -> entity.findAttributeOrNull(HiddenInventory::class)?.let {
+                    entity.removeAttribute(it)
+                    entity.addAttribute(Inventory(it.items))
+                    Consumed
+                } ?: Pass
+                is Openable -> entity.findAttributeOrNull(Inventory::class)?.let {
+                    entity.removeAttribute(it)
+                    entity.addAttribute(HiddenInventory(it.items))
+                    Consumed
+                } ?: Pass
+                else -> Pass
+            }
+        }
+
+    }
 
     fun runExample() {
         val entity = EntityBuilder.newBuilder<MyType, MyContext>(MyType)
